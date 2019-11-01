200dbad77663ecada959dafffc789ee8791b7f4b
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-11-01 17:38:51 +0200

    Add new code to App.java (scanner and random integer)

--- JPTVR18lesson1/src/myclasses/App.java
+++ JPTVR18lesson1/src/myclasses/App.java
@@ -5,13 +5,28 @@
  */
 package myclasses;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 public class App {
     public void run(){
-        System.out.println("hello, world!");
-
+        System.out.println("Привет, Юрий Мельников");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Наишите свое имя латинскими буквами");
+        String myName = scanner.nextLine(); // Работает до нажатия enter
+        System.out.println("Hi," + myName);
+        Random random = new Random();
+        int randomNumber = random.nextInt(5); // NextInt генерирует в диапозоне
+        System.out.println("Загадай число от 1 до 4");
+        int userNumber = scanner.nextInt();
+        if(userNumber == randomNumber){
+           System.out.println("Ты угадал!");
+        }else{
+            System.out.println("Не повезло, правильное число = " + randomNumber);
+        }
     }
 }
