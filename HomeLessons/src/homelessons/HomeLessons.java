d62abb5f21ae3c6189581f0158e5f05742a0f22c
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-11-27 20:18:35 +0200

    edit javaHome

--- HomeLessons/src/homelessons/HomeLessons.java
+++ HomeLessons/src/homelessons/HomeLessons.java
@@ -1,23 +1,15 @@
 package homelessons;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HomeLessons {
-
+    
+    public static String a;
+    public static String b;
+    
     public static void main(String[] args) {
-        int[] n = {1,2,3}; //Одномерный массив
-        int[][] m = {{1,2,3},{4,5,6},{7,8,9}}; //Двумерный массив
-        //Перебор массива и его элементов
-        for(int i = 0; i <= 2; i++){
-            System.out.println("Сейчас ипользуется эдемент: " + Arrays.toString(m[i]));
-            System.out.print("Его элементы: ");
-            for(int z = 0; z <= 2; z++){
-                System.out.print(m[i][z] + ", ");
-                if(z == 2){
-                    System.out.println("");
-                }
-            } 
-        }
+        
     }
     
 }
