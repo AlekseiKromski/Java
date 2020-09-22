d24bd2e623f6fe14d8f86bf1a20318e12615da03
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-22 17:03:37 +0300

    Start learn java basic

---
+++ LearnJava/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author yanikarp
+ */
+public class HelloWorld {
+
+    byte b = 0; //1 byte(256) -128 to 138
+    short s = 0; //2 byte(~65000) -32 to 32
+    int i = 0; //4 byte(2**31) 
+    long l = 0; //8 byte(2**63)
+    float f = (float) 12.33; //4 byte Приводим double к значению float
+    double d = 123.123; //8 byte
+    boolean bool = true;
+    char c;
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
