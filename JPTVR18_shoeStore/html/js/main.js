7cc999db7b669756f95e0270b2d11d3b845fc522
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-28 18:14:38 +0300

    make about us and slider section

--- JPTVR18_shoeStore/html/js/main.js
+++ JPTVR18_shoeStore/html/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $('.single-item').slick({
-        "setting-name": "setting-value"
+        "setting-name": "setting-value",
+        "arrows" : false,
+        "dots": true,
+        "fade": true,
+	    "fadeSpeed": 1000
     });
     console.log('ok');
 })
